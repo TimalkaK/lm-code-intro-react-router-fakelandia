c7f75da80723c5a4908cd7607a6b527c85ae97d7
TimalkaK <timalkamk@gmail.com>
2023-09-29 13:53:22 +0100

    fix: Removed unnecessary components and integrated a table into MisdemeanourList component

--- client/src/components/misdemeanours/misdemeanourList.tsx
+++ client/src/components/misdemeanours/misdemeanourList.tsx
@@ -1,19 +1,44 @@
 import { useContext } from 'react';
 import { MisdemeanourContext } from './misdemeanours';
 
+
 export const MisdemeanourList: React.FC = () => {
 	const misdemeanourData = useContext(MisdemeanourContext);
 
 	return (
 	<div>
+
 		<select name="misdemeanourFilter" id="misdemeanourFilter">
     <option value="Filter" selected>Filter</option>
     <option value="View">View 1</option>
 		<option value="ViewAll">View All</option>
 		</select>
-		<h3>Misdemeanours</h3>
-		<ul><li>{JSON.stringify(misdemeanourData)}</li></ul>
-		</div>
+
+		{/* <h3>Misdemeanours</h3> */}
+		 {/* {misdemeanourData.map(m => {
+  	console.log(m.misdemeanour);
+  	return (
+			<ul><li>{m.misdemeanour}</li></ul>
+  )
+})}  */}
+	<table>
+			<tr>
+			<th>Citizen ID</th>
+			<th>Date</th>
+			<th>Misdemeanours</th>
+			</tr>          
+			{misdemeanourData.map(m => {
+			return (
+			<tr>
+			<td>{m.citizenId}</td>
+			<td>{m.date}</td>
+			<td>{m.misdemeanour}</td>
+			</tr>
+			)
+			})}
+	</table> 
+
+	</div>
 )};
 
 
--- client/src/components/misdemeanours/misdemeanours.tsx
+++ client/src/components/misdemeanours/misdemeanours.tsx
@@ -1,8 +1,6 @@
 import { useEffect, useState, createContext } from "react";
 import { Misdemeanour } from "../../types/misdemeanours.types";
-import { CitizenIdList } from "./citizenIdList";
 import { MisdemeanourList } from "./misdemeanourList";
-import { DateList } from "./dateList";
 import { PunishmentIdea } from "./punishmentIdea";
 
 export const MisdemeanourContext = createContext<Array<Misdemeanour>>([{
@@ -13,32 +11,35 @@ export const MisdemeanourContext = createContext<Array<Misdemeanour>>([{
 
 export const Misdemeanours : React.FC = () => {
 
-    const [misdemeanours, setMisdemeanours] = useState<Array<Misdemeanour> | []>([]);
+    const [misdemeanours, setMisdemeanours] = useState<Array<Misdemeanour>>();
+    const [isFetching, setIsFetching ] = useState(true);
 
     useEffect(() => {
         const fetchMisdemeanour = async () =>{
             try{
                 const response = await fetch("http://localhost:8080/api/misdemeanours/3");
                 const data = await response.json();
-                console.log('Hello: ' + JSON.stringify(data.));
-                setMisdemeanours(data);
-                console.log('Misdemeanours: ' + misdemeanours);
-            }catch(error){
+                setIsFetching(false)
+                setMisdemeanours(data.misdemeanours);
+            } catch(error){
                 console.log(error);
             }
         }
-        fetchMisdemeanour();
-    },[]);
+        if(isFetching) {
+            fetchMisdemeanour();
+        }
+    },[isFetching, misdemeanours]);
 
-
+    console.log("mis:",misdemeanours)
     return (
+        <>
+        {isFetching && <p>loading</p>}
+        {(!isFetching && misdemeanours) &&   
         <div className="misdemeanour-list" style={{display: "flex"}}>
-            {/* <MisdemeanourContext.Provider value={misdemeanours}> */}
-            <CitizenIdList />
-            <DateList />
-            <MisdemeanourList />     
-            <PunishmentIdea />
-            {/* </MisdemeanourContext.Provider> */}
-        </div>
+            <MisdemeanourContext.Provider value={misdemeanours}>
+                <MisdemeanourList/>
+            </MisdemeanourContext.Provider>
+        </div>}
+        </>
     )
 }
