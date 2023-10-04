eccaef4bd991933fa8df7765ab58dfa9d3da4c9f
TimalkaK <timalkamk@gmail.com>
2023-10-04 21:04:22 +0100

    feat: Added a test to check table is rendering on misdemeanoursList component

--- client/src/components/misdemeanours/misdemeanoursList.test.tsx
+++ client/src/components/misdemeanours/misdemeanoursList.test.tsx
@@ -1,6 +1,12 @@
+import { MisdemeanourList } from "./misdemeanourList";
+import { render, screen } from '@testing-library/react';
 
 describe('Misdemeanours List', () => {
-  test('Table is appearing on the page', () => {});
+  test('Table is appearing on the page', () => {
+    render(<MisdemeanourList/>);
+    const table = screen.getByRole("table");
+    expect(table).toBeInTheDocument();
+  });
 
   test('Filter updates the list on the page', () => {});
 
